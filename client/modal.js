bf3b5fced75aacaf96aa3ce2bc2bb88642f70ddc
kangsoontan <tankangsoon@gmail.com>
2015-11-07 15:53:19 -0800

    markers not draggable. Remove test codes

--- client/modal.js
+++ client/modal.js
@@ -1,7 +1,6 @@
 Template.eventModal.helpers({
     markerInfo: function() {
     	var markerId = Session.get('markerId');
-    	console.log(Attractions.findOne(markerId));
     	return Attractions.findOne(markerId);
     }
 });
