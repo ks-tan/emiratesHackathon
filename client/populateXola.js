6a5cc01ba11c4da7a35347ce757d96e5bf0bb515
Jack Ong <jackong2013@gmail.com>
2015-11-07 15:50:31 -0800

    Populate StubHub Data

--- client/populateXola.js
+++ client/populateXola.js
@@ -2,6 +2,10 @@
 Template.populateXola.helpers({
 	populateCollections: function(){
 		getXolaExperiences("https://dev.xola.com/api/experiences?limit=100")
+	},
+
+	populateStubHubCollections: function() {
+		getStubHubData()
 	}
 });
 
@@ -30,9 +34,10 @@ getXolaExperiences = function(url){
 			}
 			Attractions.insert({
 				_id: experience.id,
-				title: experience.name,
-				description: experience.description,
-				location: experience.geo.lat + ", " + experience.geo.lng,
+				source: 'Xola',
+				title: name,
+				description: description,
+				location: location.lat + ", " + location.lng,
 				price: experience.price,
 				pictureUrl: photoUrl
 			});
@@ -44,4 +49,44 @@ getXolaExperiences = function(url){
 			getXolaExperiences(nextPageUrl)
 		}
 	});
+}
+
+getStubHubData= function(){
+    Meteor.call('populateStubHub', function(err, data) { 
+        if (typeof err != "undefined") {
+            console.log(err);
+            return
+        }
+
+        for (var state_index in data) {
+            stateData = data[state_index]
+            for (var event_index in stateData) {
+                eventData = stateData[event_index];
+                console.log(eventData);
+                var id = eventData.id;
+                var name = eventData.name;
+                var desc = eventData.description;
+                var loc = eventData.venue.latitude + ", " + eventData.venue.longitude;
+                console.log(location);
+                var ticketPrice = "NA"
+                if (typeof eventData.ticketInfo !== "undefined") {
+                    ticketPrice = eventData.ticketInfo.minPrice
+                }
+                var pictureUrl = "";
+                if (typeof eventData.imageUrl !== "undefined") {
+                    pictureUrl = eventData.imageUrl
+                }
+
+                Attractions.insert({
+                    _id: id,
+                    source: "StubHub",
+                    title: name,
+                    description: desc,
+                    location: loc,
+                    price: ticketPrice,
+                    pictureUrl: pictureUrl
+                });
+            }
+        }
+    });
 }
