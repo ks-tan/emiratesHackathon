5535b3b6e1bc989d53f6beb1c50b560673d84761
Chua Chin Siang <ccs6327@gmail.com>
2015-11-07 20:49:10 -0800

    smap it

--- client/modal.js
+++ client/modal.js
@@ -2,21 +2,15 @@ Template.eventModal.helpers({
     markerInfo: function() {
     	var markerId = Session.get('markerId');
     	return Attractions.findOne(markerId);
-    },
-    isXola: function(source){
-    	console.log(source == "Xola");
-    	return source == "Xola";
     }
 });
 
-Template.eventModal.rendered = function() {
-    var co=document.createElement("script");
-    co.type="text/javascript";
-    co.async=true;
-    co.src="https://xola.com/checkout.js";
-    var s=document.getElementsByTagName("script")[0];
-    s.parentNode.insertBefore(co, s);
-}
+Template.eventModal.events({
+	'click .smapButton': function(event){
+		var id = event.target.value;
+		
+	}
+});
 
 Template.yourModal.helpers({
     moods: function() {
