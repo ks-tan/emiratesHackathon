6fadf645bae9c664c130d567960a271bb3d80ff4
sai <sai.chue@playphone.com>
2015-11-07 17:15:05 -0800

    semantic-ui

--- client/emirates.js
+++ client/emirates.js
@@ -1,1 +1,40 @@
-emirates.js
+Template.page2.helpers({
+	generate: function () {
+		//call the skyscanner api
+	},
+	result: function() {
+		var res = [
+		    {
+		    	"FlightNo": "SQ1",
+				"FlightDateTime": "23:12",
+				"Sector": "Commercial Asia",
+				"TransitsStations": {
+				"TransitsStation": [
+					"Hong Kong"
+			        ]
+			      },
+			    "Duration": "12h",
+			    "FlightFare": "1000SGD",
+				"Currency": "SGD"
+		    },
+		    {
+		    	"FlightNo": "SQ2",
+				"FlightDateTime": "23:12",
+				"Sector": "Commercial Asia",
+				"TransitsStations": {
+				"TransitsStation": [
+					"Hong Kong"
+			        ]
+			      },
+			    "Duration": "12h",
+			    "FlightFare": "1000SGD",
+				"Currency": "SGD"
+		    }
+		]
+		return res;
+	}
+});
+
+Template.page2.events({
+
+});
