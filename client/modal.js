7d32fc52f6bb5bb6cf39d6901d6037d750eb7508
Chua Chin Siang <ccs6327@gmail.com>
2015-11-07 18:37:46 -0800

    minor changes

--- client/modal.js
+++ client/modal.js
@@ -4,16 +4,18 @@ Template.eventModal.helpers({
     	return Attractions.findOne(markerId);
     },
     isXola: function(source){
-    	console.log(source);
-    	return source === "Xola";
+    	console.log(source == "Xola");
+    	return source == "Xola";
     }
 });
 
-(function() {
+Template.eventModal.onRendered({ 
+	function() {
         var co=document.createElement("script");
         co.type="text/javascript";
         co.async=true;
         co.src="https://xola.com/checkout.js";
         var s=document.getElementsByTagName("script")[0];
         s.parentNode.insertBefore(co, s);
-    })();
+    }
+})
