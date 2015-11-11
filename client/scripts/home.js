4f3aa984d64deace7cc8f3c408fcebdd6fdf5cc2
kangsoontan <tankangsoon@gmail.com>
2015-11-10 22:53:13 -0800

    Refactor and left comments in home.js

--- client/scripts/home.js
+++ client/scripts/home.js
@@ -3,94 +3,41 @@ Meteor.startup(function() {
 });
 
 Template.home.onCreated(function() {
-    GoogleMaps.ready('map', function(map) {
-      globalMap = map;
-    	var latLng = Geolocation.latLng();
-      Session.set('my_lat', latLng.lat);
-      Session.set('my_lng', latLng.lng);
-      var image = {
-        url: "images/bluepin.png",
-        scaledSize: new google.maps.Size(35,35),
-        origin: new google.maps.Point(0,0)
-      };
-    	var marker = new google.maps.Marker({
-		   	position: new google.maps.LatLng(latLng.lat, latLng.lng),
-		   	animation: google.maps.Animation.DROP,
-		   	map: map.instance,
-        icon: image,
-        zIndex: 1000
-		  });
+  
+  GoogleMaps.ready('map', function(map) {
+    
+    globalMap = map;
+    var ownLatLng = getOwnLocation();
+    dropOwnMarker(ownLatLng, map);
+    map.instance.setOptions({ minZoom: 4, maxZoom: 15 });
 
+  	var markers = {};
+  	Attractions.find().observe({
+    	added: function (document) {
+        var marker = dropMapMarker(document, map);
+    		markers[document._id] = marker;
+      },
+    	changed: function (newDocument, oldDocument) {
+      	markers[newDocument._id].setPosition({ lat: newDocument.lat, lng: newDocument.lng });
+    	},
+    	removed: function (oldDocument) {
+        markers[oldDocument._id].setMap(null);
+        google.maps.event.clearInstanceListeners(markers[oldDocument._id]);
+        delete markers[oldDocument._id];
+    	}
+    });
+
+    //TODO: should this function even be here?
     $(".yourMood").on('click', function(event){
       $("#yourMarker").modal("show");
     });
 
-
-      	google.maps.event.addListener(map.instance, 'click', function(event) {
-      		showListModal(event);
-      	});
-
-        map.instance.setOptions({ minZoom: 4, maxZoom: 15 });
-
-      	var markers = {};
-
-      	Attractions.find().observe({
-        	added: function (document) {
-            var randomNum = Math.floor((Math.random() * 3) + 1);
-            var mood;
-            var image;
-            if (randomNum == 1) {
-              mood = "fun";
-              image = "http://www.google.com/mapfiles/ms/micons/ylw-pushpin.png"
-            } else if (randomNum == 2) {
-              mood = "romantic";
-              image = "http://www.google.com/mapfiles/ms/micons/pink-pushpin.png"
-            } else if (randomNum == 3){
-              mood = "adventure";
-              image = "http://www.google.com/mapfiles/ms/micons/blue-pushpin.png"
-            }
-        		var lat = document.latitude;
-        		var lng = document.longitude;
-
-        		var image = {
-  				    url: 'images/'+mood+'.png',
-  				    scaledSize: new google.maps.Size(45, 45),
-  				    origin: new google.maps.Point(0, 0),
-				    };
-
-
-          		var marker = new google.maps.Marker({
-            		//draggable: true,
-            		//animation: google.maps.Animation.DROP,
-            		position: new google.maps.LatLng(lat,lng),
-            		map: map.instance,
-            		icon: image,
-            		opacity: 0.3,
-            		id: document._id
-          		});
-
-	          	//click event marker
-	         //  	marker.addListener('click', function(event) {
-					     //   Session.set('markerId', marker.id);
-					     //   $("#eventMarker").modal("show");
-				      // });
-
-            marker.addListener('click', function(event) {
-               showListModal(event);
-             });
-
-          		markers[document._id] = marker;
-        	},
-        	changed: function (newDocument, oldDocument) {
-          		markers[newDocument._id].setPosition({ lat: newDocument.lat, lng: newDocument.lng });
-        	},
-        	removed: function (oldDocument) {
-		        markers[oldDocument._id].setMap(null);
-		        google.maps.event.clearInstanceListeners(markers[oldDocument._id]);
-		        delete markers[oldDocument._id];
-        	}
-      	});
+    //TODO: open up another page
+    google.maps.event.addListener(map.instance, 'click', function(event) {
+      showListModal(event);
     });
+
+  });
 });
 
 Template.home.helpers({
@@ -105,6 +52,68 @@ Template.home.helpers({
 	    }
     }
 });
+
+getOwnLocation = function() {
+  var latLng = Geolocation.latLng();
+  Session.set('my_lat', latLng.lat);
+  Session.set('my_lng', latLng.lng);
+  return latLng;
+}
+
+dropOwnMarker = function(latLng, map) {
+  var image = {
+    url: "images/bluepin.png",
+    scaledSize: new google.maps.Size(35,35),
+    origin: new google.maps.Point(0,0)
+  };
+
+  var marker = new google.maps.Marker({
+    position: new google.maps.LatLng(latLng.lat, latLng.lng),
+    animation: google.maps.Animation.DROP,
+    map: map.instance,
+    icon: image,
+    zIndex: 1000
+  });
+}
+
+dropMapMarker = function (document, map){
+
+  //TODO: do not use random!
+  var randomNum = Math.floor((Math.random() * 3) + 1);
+  var mood;
+  var image;
+
+  if (randomNum == 1) {
+    mood = "fun";
+  } else if (randomNum == 2) {
+    mood = "romantic";
+  } else if (randomNum == 3){
+    mood = "adventure";
+  }
+
+  var lat = document.latitude;
+  var lng = document.longitude;
+
+  var image = {
+    url: 'images/'+mood+'.png',
+    scaledSize: new google.maps.Size(45, 45),
+    origin: new google.maps.Point(0, 0),
+  };
+
+  var marker = new google.maps.Marker({
+    position: new google.maps.LatLng(lat,lng),
+    map: map.instance,
+    icon: image,
+    opacity: 0.3,
+    id: document._id
+  });
+
+  marker.addListener('click', function(event) {
+    showListModal(event);
+  });
+
+  return marker;
+}
 
 showListModal = function(event){
   console.log(event);
